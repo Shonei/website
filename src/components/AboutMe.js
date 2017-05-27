02fa9a48cc4be01d36970d97ff6d2c520320f225
Teodor Todorov <teodor.shyl@gmail.com>
2017-05-27 19:48:47 +0100

    Gallery now has full functionality but there is a need for gui updates on small devices

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,23 +5,17 @@ class Text extends Component {
   render() {
     return (
       <div>
-        <input type="checkbox" className="read-more-state" id="post-1" />
+        <input type="checkbox" className="read-more-state" id={this.props.id} />
         <p className="center-left read-more-wrap">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
           <span className="read-more-target">Libero fuga facilis vel consectetur quos sapiente deleniti eveniet dolores tempore eos deserunt officia quis ab? Excepturi vero tempore minus beatae voluptatem!</span>
         </p>
-        <label htmlFor="post-1" className="read-more-trigger"></label>
+        <label htmlFor={this.props.id} className="read-more-trigger"></label>
       </div>
     );
   }
 }
 
 class AboutMe extends Component {
-
-  constructor(){
-    super();
-
-    this.text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`;
-  }
 
   render() {
     return (
@@ -36,28 +30,22 @@ class AboutMe extends Component {
           </div>
           <div className="col s12 m12 l6">
             <h4 className="center-align">Some text</h4>
-            <Text />
+            <Text id={"text1"} />
           </div>
         </div>  
         <div className="divider"></div>
         <div className="row section">
           <div className="col s12 m12 l6">
             <h4 className="<center-align></center-align>">Some text</h4>
-            <Text />
+            <Text id={"text2"}/>
           </div>
           <div className="col s12 m12 l6">
             <img src="aPB2KbP_700b.jpg" alt="profile" className="responsive-img"/>
           </div>
-        </div>
-        <div className="divider"></div>
-        <div className="row section">
-            <div className="video-container">
-            </div>
         </div>
       </div>
     );
   }
 }
 
-              // <iframe title="vid" width="854" height="480" src="https://www.youtube.com/embed/BsSmBPmPeYQ" frameBorder="0" allowFullScreen></iframe>
 export default AboutMe;
--- src/components/Eggs.js
+++ src/components/Eggs.js
@@ -11,21 +11,22 @@ class Eggs extends Component {
 		}
 
     this.database = this.props.dataStorage.database();
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  componentDidMount() {
     this.database.ref('/img/aboutMe/').once('value').then(snapshot => {
       const urls = snapshot.val();
 
       const arr = [];
 
       for(let i in urls){
-        const img = <img className="responsive-img" src={urls[i]} alt={i}/> 
+        const img = <img className="responsive-img image" src={urls[i]} alt={i}/> 
         arr.push(img);
       };
       
       this.setState({images: arr});
     });
-
-    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(index) {
@@ -35,11 +36,22 @@ class Eggs extends Component {
   render() {
     return (
       <div>
-        <Thumbnails 
-          imageSet={this.state.images}
-          action={this.handleClick}/>
-        <LargeImage 
-          image={this.state.images[this.state.currentImageIndex]}/>
+        <div className="card purple lighten-4">
+          <div className="card-content">
+            <Thumbnails 
+              imageSet={this.state.images}
+              action={this.handleClick}
+              largeImageIndex={this.state.currentImageIndex}/>
+            </div>
+            <div className="divider"></div>
+            <div className="card-image">
+            <LargeImage 
+              image={this.state.images[this.state.currentImageIndex]}
+              currentIndex={this.state.currentImageIndex}
+              totalImages={this.state.images.length}
+              action={this.handleClick}/>
+          </div>
+        </div>
       </div>
     );
   }
@@ -50,11 +62,33 @@ class Thumbnails extends Component {
     super(props);
     
     this.handleClick = this.handleClick.bind(this);
-  }
+    this.makeThumbnail = this.makeThumbnail.bind(this);
+  };
 
   handleClick() {
     this.props.action(1);
-  }
+  };
+
+  makeThumbnail(index, highlight = false) {
+    let i;
+    let length = this.props.imageSet.length; 
+
+    if(index >= length) {
+      i = (index === length) ? 0 : 1;
+    } else if(index < 0) {
+      i = length - 1;
+    } else {
+      i = index;
+    }
+
+    return (
+        <ThumbnailImage 
+          action={this.props.action}
+          image={this.props.imageSet[i]}
+          index={i} 
+          highlight={highlight}/>
+      );
+  };
 
   render() {
     return (
@@ -62,22 +96,10 @@ class Thumbnails extends Component {
         <div className="row section">
           <div className="valign-wrapper">
             <div className="col s2"></div>
-              <ThumbnailImage 
-                action={this.props.action}
-                imageSet={this.props.imageSet}
-                index={0} />
-              <ThumbnailImage 
-                action={this.props.action}
-                imageSet={this.props.imageSet}
-                index={1} />
-              <ThumbnailImage 
-                action={this.props.action}
-                imageSet={this.props.imageSet}
-                index={2} />
-              <ThumbnailImage 
-                action={this.props.action}
-                imageSet={this.props.imageSet}
-                index={0} />
+              {this.makeThumbnail(this.props.largeImageIndex-1)}  
+              {this.makeThumbnail(this.props.largeImageIndex, true)}
+              {this.makeThumbnail(this.props.largeImageIndex+1)}
+              {this.makeThumbnail(this.props.largeImageIndex+2)}
             <div className="col s2"></div>
           </div>
         </div>  
@@ -99,9 +121,11 @@ class ThumbnailImage extends Component {
 
   render() {
     return (
-      <div className="col s2">
+      <div 
+        className={this.props.highlight ? "col s2 z-depth-5" : "col s2"}
+        id={this.props.highlight ? 'activeImage' : ''}>
         <a onClick={this.handleClick}>
-          {this.props.imageSet[this.props.index]}
+          {this.props.image}
         </a>
       </div>
     );
@@ -111,17 +135,80 @@ class ThumbnailImage extends Component {
 class LargeImage extends Component {
   constructor(props) {
     super(props);
+  
+    this.handleLeftClick = this.handleLeftClick.bind(this);
+    this.handleRightClick = this.handleRightClick.bind(this);
+    this.nextImageIndex = this.nextImageIndex.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+  };
+
+  nextImageIndex(index) {
+    const length = this.props.totalImages;
+    let i;
+
+    if(index >= length) {
+      i = 0;
+    } else if(index < 0) {
+      i = length - 1;
+    } else {
+      i = index;
+    }
+
+    return i;
+  }
+
+  handleLeftClick() {
+    this.props.action(this.nextImageIndex(this.props.currentIndex-1));
+  }
+
+  handleRightClick() {
+    this.props.action(this.nextImageIndex(this.props.currentIndex+1));
+  }
+
+  handleKeyPress(event) {
+    if(event.keyCode === 37) {
+      this.handleLeftClick();
+    } else if(event.keyCode === 39) {
+      this.handleRightClick();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+    document.body.style.backgroundColor = '#f3e5f5';
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+    document.body.style.backgroundColor = 'white';
   };
 
   render() {
     return (
         <div>
           <div className="row selection">
-            <div className="col s3"></div>
+            <div className="col s1"></div>
+            <div className="col s1">
+              <button 
+                className="btn waves-effect waves-light"
+                onClick={this.handleLeftClick}
+                onKeyDown={this.handleKeyPress}
+                >Prev</button>
+            </div>
+            <div className="col s1"></div>
             <div className="col s6 center-align">
               {this.props.image}
             </div> 
-            <div className=" col s3"></div>
+            <div className="col s1"></div>
+            <div className="col s1">
+              <button 
+                className="btn waves-effect waves-light"
+                onClick={this.handleRightClick}
+              >Next</button>
+            </div>
+            <div className=" col s1"></div>
           </div>
         </div>
       )
